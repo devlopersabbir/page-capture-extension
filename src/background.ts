0c12771cb2be93b598f2cfc2e5c995f9bc3ad7a7
devlopersabbir <devlopersabbir@gmail.com>
2023-06-03 04:49:12 +0600

    error when trying to take full screen

--- src/background.ts
+++ src/background.ts
@@ -1,25 +1,30 @@
 chrome.action.onClicked.addListener(async (tab: any) => {
-  const fullPageScreenshotData = await cupturePageDimension(tab?.id);
-  console.log("Full-page screenshot captured:", fullPageScreenshotData);
+  try {
+    await capturePageDimension(tab?.id);
 
-  chrome.notifications.create({
-    type: "basic",
-    iconUrl: "icon.png",
-    title: "Screenshot Finished",
-    message: `Captured dimensions:`,
-  });
-  // for taken a screnshot
-  chrome.tabs.captureVisibleTab({ format: "png" }, (screenshotURL) => {
-    console.log("url got it");
-  });
+    // console.log("Full-page screenshot captured:", fullPageScreenshotData);
+
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "Screenshot Finished",
+      message: `Captured dimensions: `,
+    });
+    // for taken a screnshot
+    chrome.tabs.captureVisibleTab({ format: "png" }, (screenshotURL) => {
+      console.log("url got it", screenshotURL);
+    });
+  } catch (error) {
+    console.log("error...", error);
+  }
 
   // send a notification to make sure screenshot is created
 });
 
-const cupturePageDimension = (tabId: number) => {
+const capturePageDimension = (tabId: number) => {
   return new Promise((resolve) => {
     chrome.tabs.sendMessage(tabId, { action: "cupturePage" }, (response) => {
-      resolve(response);
+      console.log(response);
     });
   });
 };
